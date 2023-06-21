d42d54eb4bbbcae446dd8d2e37d69b7314276a33
hoalasama <hoalasamaeu@gmail.com>
2023-06-21 10:08:16 +0700

    'how'

--- forumweb/main/urls.py
+++ forumweb/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, detail, posts, create_post
+from .views import home, detail, posts, create_post, latest_posts
 
 urlpatterns = [
     path("", home, name="home"),
     path("detail/<slug>/", detail, name="detail"),
     path("posts/<slug>/", posts, name="posts"),
     path("create_post", create_post, name="create_post"),
+    path("latest_posts", latest_posts, name="latest_posts"),
+
 ]
--- forumweb/static/main.js
+++ forumweb/static/main.js
@@ -13,12 +13,14 @@ function showIconBar(){
     navigation.classList.add("hide");
 }
 
+//Comment
 function showComment(){
     var commentArea = document.getElementById("comment-area");
     commentArea.classList.remove("hide");
 }
 
-function showReply(){
-    var replyArea = document.getElementById("reply-area");
+//Reply
+function showReplies(id){
+    var replyArea = document.getElementById(id);
     replyArea.classList.remove("hide");
 }
